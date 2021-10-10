503e66be540b1efc42fdadf5719e40a3c5ec7086
TheQwertiest <qwertiest@mail.ru>
2021-10-10 20:24:49 +0300

    Added component version attribute

---
+++ dotnet_component_interface/ComponentInterfaceVersionAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Qwr.ComponentInterface
+{
+    [AttributeUsage(AttributeTargets.Constructor)]
+    public class ComponentInterfaceVersionAttribute : Attribute
+    {
+        public readonly Version Version;
+        public ComponentInterfaceVersionAttribute(string version)
+        {
+            this.Version = new Version(version);
+        }
+    }
+}
